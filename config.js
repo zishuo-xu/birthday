48b09c2fc1940262d8fd73da555ad075bf40e2f0
zishuo-xu <67620191+zishuo-xu@users.noreply.github.com>
2023-05-18 16:42:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -41,9 +41,9 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "生日",
+        turn_on: "开灯",
+        play: "来点音乐",
+        bannar_coming: "生日快乐",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
